9626b51eed0535a7b0ece7da75fb150b1314e63f
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-04-14 22:13:27 -0400

    heroku

---
+++ client/build/precache-manifest.690629403240c6d8603c9982f7f39dc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b38ef0a1cfab8b3c172c",
+    "url": "/static/js/main.06f28012.chunk.js"
+  },
+  {
+    "revision": "04a82d45522a69a599ab",
+    "url": "/static/js/2.3129917b.chunk.js"
+  },
+  {
+    "revision": "b38ef0a1cfab8b3c172c",
+    "url": "/static/css/main.7916a9bc.chunk.css"
+  },
+  {
+    "revision": "377c9bb16eb6544800f09cd778f10812",
+    "url": "/index.html"
+  }
+];
